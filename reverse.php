59f8ae1a113ef7f2750d572bd741f9240dd3cee5
Owen_Gittins <99190962+OwenGitt@users.noreply.github.com>
2023-07-01 16:42:06 +0100

    Create reverse.php

---
+++ reverse.php
@@ -0,0 +1,18 @@
+<?php 
+function reverse($stringToReverse) {
+	if(strlen($stringToReverse) == 1) {
+	return $stringToReverse;
+}
+else{
+$endChar = substr($stringToReverse, -1);
+	$restOfString = substr($stringToReverse, 0, -1);
+	return $endChar.=reverse($restOfString);		
+}
+}
+
+$initialString = "Roxoa Group";
+$reversedString = reverse($initialString);
+echo $reversedString;
+
+
+?>
