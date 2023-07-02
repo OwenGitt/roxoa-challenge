1d86232c6bb2ff91d1a1e7d8e26812e9f6c59627
Owen_Gittins <99190962+OwenGitt@users.noreply.github.com>
2023-07-02 14:42:00 +0100

    Update coffeeClass.php

--- coffeeClass.php
+++ coffeeClass.php
@@ -3,12 +3,44 @@
 	class coffee {
 		
 		
-		function __construct($milk, $espresso, $sugar) {
-			
-			
+		function __construct($milk, $art) {
+			$this->boilCoffeeWater();
+			$this->grindCoffeeBeans();
+            $this->pressGroundCoffeeBeans();
+            $this->makeCoffee();
+            $this->addMilk($milk, $art);
 		}
 		
-	
+        private function boilCoffeeWater() {
+           echo "Boiling water for the coffee";
+           sleep(2);
+           echo "Water boiled, waiting for it to cool";
+           sleep(1);
+           echo "Water has reached an appropriate temperature";
+        }
+        
+        private function grindCoffeeBeans() {
+           echo "Grinding coffee beans";
+           sleep(2);
+           echo "Coffee beans grounded successfully";
+
+        }
+        
+        private function pressGroundCoffeeBeans() {
+           echo "Pressing your ground coffee beans in the machine now";
+           sleep(1);
+           echo "Machine is heating up now";
+        }
+        
+        private function makeCoffee() {
+           echo "Coffe is pouring, it looks amazing!";
+
+        }
+        
+        private function addMilk($milk, $art) {
+           echo "Adding ".$milk."now and making a ".$art." with it";
+            sleep(2);
+        }
 		
 
 	public function giveCoffee() { //Displays coffee
